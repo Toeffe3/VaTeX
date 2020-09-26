fd67f3565d130761a276ccca9900e9bf2400e900
Victor Vejlgaard Schelde Jacobsen <densejevic@gmail.com>
2020-09-27 00:57:35 +0200

    Created general structure

---
+++ header.php
@@ -0,0 +1,9 @@
+<?php  ?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>VaTeX - Live Group Tex Editor</title>
+    <link rel="stylesheet" href="assets/access.css">
+  </head>
+  <body>
