505ba8ec834932ba5eddb9200a6fd1905dc0e95b
Victor Vejlgaard Schelde Jacobsen <densejevic@gmail.com>
2020-10-07 21:57:14 +0200

    Front page frontend done

--- header.php
+++ header.php
@@ -4,6 +4,7 @@
   <head>
     <meta charset="utf-8">
     <title>VaTeX - Live Group Tex Editor</title>
+    <link rel="icon" href="assets/VaTeX-pink-ico.png">
     <link rel="stylesheet" href="assets/access.css">
   </head>
   <body>
